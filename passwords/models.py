db43989b44e851ba90de937485c059eef4ba0f1f
zorojurio <zorojurio@gmail.com>
2021-12-16 23:01:12 +0530

    models of the password manager

--- passwords/models.py
+++ passwords/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from platforms.models import Platform
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+class PlatformPassword(models.Model):
+    password = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.user.username
+    
+    
+    
--- platforms/models.py
+++ platforms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.query_utils import select_related_descend
 
-# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    
