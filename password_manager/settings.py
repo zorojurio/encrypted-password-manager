156422b4da5bef8939ac10f3ab31643f2a690809
zorojurio <zorojurio@gmail.com>
2021-12-16 18:27:26 +0530

    custom user model

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'users'
 ]
 
+AUTH_USER_MODEL = 'users.CustomUser' # new
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
